7fc7210f0249acc21be0b1902a0c0c626385f716
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-03-02 11:44:41 +0800

    chore: bump version 0.6.3

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         V2EX Base64 Helper
 // @name:zh-CN   V2EX Base64 助手
 // @namespace    https://github.com/amorphobia/v2ex-base64-helper
-// @version      0.6.2
+// @version      0.6.3
 // @description  Base64 auto decoding, reply with base64 encoded
 // @description:zh-CN  Base64 自动解码，用 base64 编码回复
 // @author       Hinnka, amorphobia
